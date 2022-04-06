48143a9a9f52e24b58b3df40f50eb422c6160a87
Grant Ramsay <seapagan@gmail.com>
2022-04-06 10:44:15 +0000

    initial commit, static pages

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  root 'static_pages#landing_page'
+  get 'privacy_policy', to: 'static_pages#privacy_policy'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
