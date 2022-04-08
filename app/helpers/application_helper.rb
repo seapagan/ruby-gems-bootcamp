b7a02619432c7e434daca89d429abb7ce1c366b6
Grant Ramsay <seapagan@gmail.com>
2022-04-08 15:18:42 +0000

    style user list and detail views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def boolean_label(value)
+    case value
+    when true
+      content_tag(:span, value.to_s.titleize, class: 'badge badge-success')
+    when false
+      content_tag(:span, value.to_s.titleize, class: 'badge badge-danger')
+    end
+  end
 end
