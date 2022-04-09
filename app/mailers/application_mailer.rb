d22436e5da1991dbba429dc94920f2b85da70ea6
Grant Ramsay <seapagan@gmail.com>
2022-04-09 11:31:42 +0000

    add test email settings for production

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'seapagan@seapagan.net'
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,23 @@
-require "active_support/core_ext/integer/time"
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+
+  # mail settings
+  config.action_mailer.default_url_options = { host: 'rails-course.seapagan.net/',
+                                               protocol: 'https' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port: 587,
+    address: 'email-smtp.eu-west-1.amazonaws.com',
+    user_name: Rails.application.credentials.dig(:sesemail, :user),
+    password: Rails.application.credentials.dig(:sesemail, :password),
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # for devise
   config.action_mailer.default_url_options = { host: 'rails-course.seapagan.net' }
@@ -56,7 +72,7 @@ Rails.application.configure do
   config.log_level = :info
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
@@ -91,7 +107,7 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
+  if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
