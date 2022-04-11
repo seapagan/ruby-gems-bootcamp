273a40fb10e564b797537019ad447a2a93786675
Grant Ramsay <seapagan@gmail.com>
2022-04-11 13:20:49 +0000

    add services

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,5 +1,6 @@
 class Classroom < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :name, uniqueness: { case_sensitive: false }
 
   def to_s
     name
