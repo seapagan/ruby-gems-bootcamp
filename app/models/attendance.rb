181e9306963c530279157d7ccad13570c249672c
Grant Ramsay <seapagan@gmail.com>
2022-04-13 14:44:35 +0000

    create attendance table and validations

---
+++ app/models/attendance.rb
@@ -0,0 +1,16 @@
+class Attendance < ApplicationRecord
+  belongs_to :user
+  belongs_to :lesson
+
+  validates :status, presence: true
+
+  STATUSES = [:planned, :attended, :not_attended, :a, :b, :c, :d, :e, :f]
+
+  def self.statuses
+    STATUSES.map { |status| [status, status] }
+  end
+
+  def to_s
+    user
+  end
+end
