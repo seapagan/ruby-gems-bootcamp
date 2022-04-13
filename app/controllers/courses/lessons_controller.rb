456ea594fd6cec205e247a05cc216f7b18b483ab
Grant Ramsay <seapagan@gmail.com>
2022-04-13 18:28:05 +0000

    nest lessons inside course

---
+++ app/controllers/courses/lessons_controller.rb
@@ -0,0 +1,56 @@
+class Courses::LessonsController < ApplicationController
+  before_action :set_course
+  before_action :set_lesson, only: %i[show edit update destroy]
+
+  # GET /lessons/new
+  def new
+    @lesson = Lesson.new
+  end
+
+  # GET /lessons/1/edit
+  def edit; end
+
+  # POST /lessons or /lessons.json
+  def create
+    @lesson = Lesson.new(lesson_params)
+
+    if @lesson.save
+      redirect_to @course, notice: 'Lesson was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /lessons/1 or /lessons/1.json
+  def update
+    if @lesson.update(lesson_params)
+      redirect_to @course, notice: 'Lesson was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /lessons/1 or /lessons/1.json
+  def destroy
+    @lesson.destroy
+    redirect_to @course, notice: 'Lesson was successfully destroyed.'
+  end
+
+  private
+
+  def set_course
+    @course = Course.find(params[:course_id])
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_lesson
+    @lesson = Lesson.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def lesson_params
+    params.require(:lesson).permit(:user_id, :classroom_id, :course_id,
+                                   :status, :start,
+                                   attendances_attributes: [:id, :user_id, :status, :_destroy])
+  end
+end
