51f733db2d2141173283bc5163becd5a30130398
Grant Ramsay <seapagan@gmail.com>
2022-04-13 17:42:23 +0000

    style statuses using a helper function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,15 @@ module ApplicationHelper
       content_tag(:span, value.to_s.titleize, class: 'badge badge-danger')
     end
   end
+
+  def status_label(status)
+    case status
+    when 'planned'
+      content_tag(:span, status.titleize, class: 'badge badge-warning')
+    when 'confirmed', 'attended'
+      content_tag(:span, status.titleize, class: 'badge badge-success')
+    when 'cancelled', 'not_attended'
+      content_tag(:span, status.titleize, class: 'badge badge-danger')
+    end
+  end
 end
